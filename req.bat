f26a6bc4967b7be43b63819f7755173fb28e173c
0xmrjoex <0xmrjoex1@gmail.com>
2025-03-19 15:00:12 -1000

    Add files via upload

---
+++ req.bat
@@ -0,0 +1,8 @@
+python -m pip install colorama
+python -m pip install colorist
+python -m pip install datetime
+python -m pip install requests
+python -m pip install tls-client
+python -m pip install websocket-client
+python -m pip install uuid
+python -m pip install typing_extensions
